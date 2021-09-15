141a5fff5727387256a8dd8fc840664e079246f2
lixianchun <2629469408@qq.com>
2021-09-15 21:56:42 +0800

    learn

---
+++ dataType.py
@@ -0,0 +1,35 @@
+# 数据类型
+# 字符串
+str1 = 'abc'
+str2 = "abc"
+str3 = '''abc'''
+print(str1)
+print(str2)
+print(str3)
+
+str1 = "'abc'""
+str2 = '''"abc"'''
+print(str1)
+print(str2)
+
+str1 = '\'abc\''
+str2 = '\"abc\"'
+print(str1)
+print(str2)
+
+# 整数，没有小数点的数字
+int1 = 100
+int2 = 1 / 2
+print(int1)
+print(int2)
+print(type(int1))
+print(type(int2))
+
+# 浮点数，带小数的数字，是不精确的
+print(0.59 + 0.4)
+
+# 布尔值，True、False 注意大小写
+print(True)
+
+# 空值 None
+print(None)
