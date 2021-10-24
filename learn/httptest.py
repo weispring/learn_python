7f440785f36b0ff862ecb9b25fffc19ab7200a4c
lixianchun <2629469408@qq.com>
2021-10-24 21:25:27 +0800

    learn py base

---
+++ learn/httptest.py
@@ -0,0 +1,15 @@
+import urllib.request
+import urllib.parse
+import requests
+#pip install requests
+resp = urllib.request.urlopen("http://www.baidu.com/")
+html = resp.read().decode("utf-8")
+
+url = "https://www.baidu.com/"
+resp=requests.get(url)
+print(resp.text)
+
+url = "https://www.baidu.com/"
+postdatas={"k":"v"}
+resp=requests.post(url, postdatas)
+print(resp.text)
