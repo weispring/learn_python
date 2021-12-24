0dddfa975baf88460c4f61685a8e0e1c9306ccf7
lixianchun <2629469408@qq.com>
2021-12-24 18:01:16 +0800

    learn module: explame

---
+++ demo/explamedemo/autoClick.py
@@ -0,0 +1,12 @@
+﻿import pyautogui
+import pyperclip
+import time
+time.sleep(5)
+for s in range(0, 15):
+    print(pyautogui.position())
+    #pyautogui.leftClick(188, 575) # 已左上角为(0,0)
+    #pyautogui.leftClick(237, 251) # 已左上角为(0,0)
+
+    pyautogui.leftClick(240, 525) # 已左上角为(0,0)
+    pyautogui.leftClick(273,233) # 已左上角为(0,0)
+    time.sleep(3)
