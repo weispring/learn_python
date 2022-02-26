22c44da641d46eff6720a4c3245c7e9cbd5da568
lixianchun <2629469408@qq.com>
2022-02-26 15:40:33 +0800

    learn module: selenium

---
+++ demo/package2/download.py
@@ -0,0 +1,23 @@
+﻿from urllib.request import urlopen
+from urllib.request import quote, unquote, Request
+from  urllib.error import HTTPError
+import urllib.parse
+import requests
+try:
+    fileName = "3std-bsc.zip"
+    url = 'https://feature.kingdee.com:1026/cosmic-cd-fi/devfi/test/prepare/runtime/appstore/biz/3std-bsc.zip'  # 菜鸟教程搜索页面
+    header = {
+        'User-Agent':'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    }   #头部信息
+
+    r = requests.get(url) # 发送请求
+    # 保存
+    with open (fileName, 'wb') as f:
+        f.write(r.content)
+        f.close
+except HTTPError as e:
+    print(e)
+    if e.code == 404:
+        print(404)   # 404
+
+
---
+++ demo/trainticket.py
@@ -0,0 +1,36 @@
+﻿from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = None
+
+fromAddress = "深圳"
+toAddress = "武汉"
+departureTime = "2022-02-27";
+
+
+try:
+    # 需要先下载浏览器驱动 鼠标、键盘
+    chromedriver = "D:\dependenc\chromedriver_win32\chromedriver.exe"
+    driver = webdriver.Chrome(chromedriver)
+
+    driver.get("https://www.12306.cn/index/index.html")
+
+    h = driver.current_window_handle
+
+    print(h)  # 打印首页句柄
+    # 获取dom 元素api
+    fromStationText = driver.find_element_by_id("fromStationText")
+    #search.send_keys("webdriver" + Keys.ENTER)
+    action = webdriver.ActionChains(driver)
+
+
+    action.click(fromAddress).__setattr__("value", fromAddress)
+
+
+
+
+    # 获取当前的handle名字
+    handle = driver.current_window_handle
+    print("当前的handle：" ,handle)
+finally:
+    driver.quit()
